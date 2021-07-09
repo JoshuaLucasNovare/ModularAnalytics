d66b6949ec86fafd85ebb48068a6c6e324a2949a
JoshuaLucasNovare <joshua.lucas@novare.com.hk>
2021-07-09 08:59:10 +0800

    Removed the commented codes in Machine Learning 070921-0858

--- machinelearning.py
+++ machinelearning.py
@@ -25,72 +25,46 @@ def show_machinelearning_analysis(df):
             '''This function is made to test multiple training models'''
             global accuracy_scores
             if predictor == 'LogisticRegression':
-                #st.title('Training Logistic Regression on Training Set')
                 from sklearn.linear_model import LogisticRegression
                 model = LogisticRegression(**params)
 
             elif predictor == 'SVC':
-                #st.title('Training Support Vector Machine on Training Set')
                 from sklearn.svm import SVC
                 model = SVC(**params)
             
             elif predictor == 'K-SVC':
-                #st.title('Training Kernel Support Vector Machine on Training Set')
                 from sklearn.svm import SVC
                 model = SVC(**params)
 
             elif predictor == 'KNN':
-                #st.title('Training K-Nearest Neighbours on Training Set')
                 from sklearn.neighbors import KNeighborsClassifier
                 model = KNeighborsClassifier(**params)
 
             elif predictor == 'Decision Tree':
-                #st.title('Training Decision Tree Model on Training Set')
                 from sklearn.tree import DecisionTreeClassifier
                 model = DecisionTreeClassifier(**params)
 
             elif predictor == 'Gaussian NB':
-                #st.title('Training Naive Bayes Model on Training Set')
                 from sklearn.naive_bayes import GaussianNB
                 model = GaussianNB(**params)
                 
             elif predictor == 'Random Forest':
-                #st.title('Training Random Forest Model on Training Set')
                 from sklearn.ensemble import RandomForestClassifier
-                model = RandomForestClassifier(**params)
-            else:
-                #st.write('Invalid Predictor!')
-                exit    
+                model = RandomForestClassifier(**params)   
 
             model.fit(X_train, y_train)
-            
-            #filename = "something_" + predictor + ".pkl"
-            #pickle.dump(model, open(filename, 'wb'))
 
-            #st.subheader('''Prediciting Test Set Result''')
+            from sklearn.metrics import accuracy_score
             y_pred = model.predict(X_test)
-            result = np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1)
-            #st.write(result,'\n')
 
-            #st.subheader('''Making Confusion Matrix''')
-            from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
-            y_pred = model.predict(X_test)
-            cm = confusion_matrix(y_test, y_pred)
-            #st.write(cm,'\n')
-
-            #st.subheader('''Classification Report''')
-            #st.text('Model Report:\n ' + classification_report(y_test, y_pred))
-
-           # st.subheader('''Evaluating Model Performance''')
+           #Model Performance Accuracy
             accuracy = accuracy_score(y_test, y_pred)
-            #st.write("Accuracy: {:.2f} %".format(accuracy.mean()*100))
             model_scores[predictor] = accuracy.mean()*100
-            #st.subheader('''Applying K-Fold Cross validation''')
+
+            #K-Fold Cross Validation Accuracy
             from sklearn.model_selection import cross_val_score
             accuracies = cross_val_score(estimator=model, X=X_train, y=y_train, cv=10)
-            #st.write("Accuracy: {:.2f} %".format(accuracies.mean()*100))
             accuracy_scores[predictor] = accuracies.mean()*100
-            #st.write("Standard Deviation: {:.2f} %".format(accuracies.std()*100),'\n') 
         
         predictor('LogisticRegression', {'penalty': 'l1', 'solver': 'saga', 'max_iter': 5000})
         predictor('SVC', {'C': 1, 'gamma': 0.8,'kernel': 'linear', 'random_state': 0})
@@ -100,14 +74,13 @@ def show_machinelearning_analysis(df):
         predictor('Gaussian NB', {})
         predictor('Random Forest', {'criterion': 'entropy', 'max_features': 'auto', 'n_estimators': 250,'random_state': 0})
 
-
+        #Create the bar graph that compares the accuracy of each models
         def createFig(values):
             modelDf = pd.Series(values, name='Accuracy Score')
-            modelDf.index.name = 'Models'
+            modelDf.index.name = 'Model'
             modelDf.reset_index()
             fig1 = px.bar(modelDf, x=modelDf.index, y=modelDf.name)
             st.write(fig1)
-        
         
         st.subheader('Model Performance Comparison')
         createFig(model_scores)
